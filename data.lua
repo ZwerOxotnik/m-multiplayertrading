5acb451d1438016a8fbf6d5be6ffab4fe2f4e19f
ZwerOxotnik <zweroxotnik@gmail.com>
2019-11-09 20:05:47 +0300

    Update to 0.3.4

--- data.lua
+++ data.lua
@@ -1,37 +1,119 @@
 SPECIALIZATIONS = require "specializations-data"
 
-local function copy(thing)
-    if type(thing) == 'table' then
-        local result = {}
-        for key, value in pairs(thing) do
-            result[key] = copy(value)
-        end
-        return result
-    else
-        return thing
-    end
-end
-
-local buyBox = copy(data.raw.container['steel-chest'])
+local GlobalMarket = util.table.deepcopy(data.raw.market['market'])
+GlobalMarket.name = "global-market"
+GlobalMarket.allow_access_to_all_forces = false
+GlobalMarket.minable = {mining_time = 2, result = "global-market"}
+GlobalMarket.max_health = 600
+GlobalMarket.resistances = {
+    {
+        type = "fire",
+        percent = 90
+    },
+    {
+        type = "impact",
+        percent = 60
+    }
+}
+local buyBox = util.table.deepcopy(data.raw.container['steel-chest'])
 buyBox.name = "buy-box"
 buyBox.icon = "__m-multiplayertrading__/graphics/icons/buy-box.png"
 buyBox.picture.filename = "__m-multiplayertrading__/graphics/entity/buy-box.png"
-buyBox.inventory_size = 1
-local buyBoxItem = copy(data.raw.item['steel-chest'])
+buyBox.picture = {
+    layers =
+    {
+        {
+            filename = "__m-multiplayertrading__/graphics/entity/buy-box.png",
+            priority = "extra-high",
+            width = 34,
+            height = 40,
+            shift = util.by_pixel(0, -3),
+            hr_version =
+            {
+                filename = "__m-multiplayertrading__/graphics/entity/hr-buy-box.png",
+                priority = "extra-high",
+                width = 68,
+                height = 79,
+                shift = util.by_pixel(0, -3),
+                scale = 0.5
+            }
+        },
+        {
+            filename = "__base__/graphics/entity/compilatron-chest/compilatron-chest-shadow.png",
+            priority = "extra-high",
+            width = 57,
+            height = 21,
+            shift = util.by_pixel(12, 6),
+            draw_as_shadow = true,
+            hr_version =
+            {
+                filename = "__base__/graphics/entity/compilatron-chest/hr-compilatron-chest-shadow.png",
+                priority = "extra-high",
+                width = 114,
+                height = 41,
+                shift = util.by_pixel(12, 6),
+                draw_as_shadow = true,
+                scale = 0.5
+            }
+        }
+    }
+}
+local buyBoxItem = util.table.deepcopy(data.raw.item['steel-chest'])
 buyBoxItem.name = "buy-box"
 buyBoxItem.icon = "__m-multiplayertrading__/graphics/icons/buy-box.png"
 buyBoxItem.place_result = "buy-box"
-local sellBox = copy(data.raw.container['steel-chest'])
+buyBoxItem.order = "a[items]-c[steel-chest]-z[buy-box]"
+local sellBox = util.table.deepcopy(data.raw.container['steel-chest'])
 sellBox.name = "sell-box"
 sellBox.icon = "__m-multiplayertrading__/graphics/icons/sell-box.png"
+sellBox.picture = {
+    layers =
+    {
+        {
+            filename = "__m-multiplayertrading__/graphics/entity/sell-box.png",
+            priority = "extra-high",
+            width = 34,
+            height = 40,
+            shift = util.by_pixel(0, -3),
+            hr_version =
+            {
+                filename = "__m-multiplayertrading__/graphics/entity/hr-sell-box.png",
+                priority = "extra-high",
+                width = 68,
+                height = 79,
+                shift = util.by_pixel(0, -3),
+                scale = 0.5
+            }
+        },
+        {
+            filename = "__base__/graphics/entity/compilatron-chest/compilatron-chest-shadow.png",
+            priority = "extra-high",
+            width = 57,
+            height = 21,
+            shift = util.by_pixel(12, 6),
+            draw_as_shadow = true,
+            hr_version =
+            {
+                filename = "__base__/graphics/entity/compilatron-chest/hr-compilatron-chest-shadow.png",
+                priority = "extra-high",
+                width = 114,
+                height = 41,
+                shift = util.by_pixel(12, 6),
+                draw_as_shadow = true,
+                scale = 0.5
+            }
+        }
+    }
+}
 sellBox.picture.filename = "__m-multiplayertrading__/graphics/entity/sell-box.png"
 sellBox.inventory_size = 1
-local sellBoxItem = copy(data.raw.item['steel-chest'])
+local sellBoxItem = util.table.deepcopy(data.raw.item['steel-chest'])
 sellBoxItem.name = "sell-box"
 sellBoxItem.icon = "__m-multiplayertrading__/graphics/icons/sell-box.png"
 sellBoxItem.place_result = "sell-box"
+sellBoxItem.order = "a[items]-c[steel-chest]-z[sell-box]"
 
-local creditMint = copy(data.raw.radar['radar'])
+local creditMint = util.table.deepcopy(data.raw.radar['radar'])
 creditMint.name = "credit-mint"
 creditMint.energy_usage = settings.startup['credit-mint-energy-usage'].value
 
@@ -60,6 +142,7 @@ local electricTradingStation = {
 }
 
 data:extend{
+    GlobalMarket,
     sellBoxItem,
     buyBoxItem,
     sellBox,
@@ -110,9 +193,21 @@ data:extend{
         result = "electric-trading-station"
     },
     {
+        type = "recipe",
+        name = "global-market",
+        ingredients =
+        {
+          {"steel-chest", 9},
+          {"electronic-circuit", 20},
+          {"advanced-circuit", 5}
+        },
+        energy_required = 10,
+        result = "global-market"
+    },
+    {
         type = "item",
         name = "credit-mint",
-        place_result = "credit-mint", 
+        place_result = "credit-mint",
         subgroup = "extraction-machine",
         stack_size = 1,
         icon = "__base__/graphics/icons/coin.png",
@@ -121,10 +216,22 @@ data:extend{
     {
         type = "item",
         name = "electric-trading-station",
-        place_result = "electric-trading-station", 
+        place_result = "electric-trading-station",
         subgroup = "energy-pipe-distribution",
         stack_size = 100,
         icon = "__base__/graphics/icons/substation.png",
+        icon_size = 32
+    },
+    {
+        type = "item",
+        name = "global-market",
+        place_result = "global-market",
+        subgroup = "extraction-machine",
+        stack_size = 1,
+        icons = {
+            {icon = "__base__/graphics/icons/infinity-chest.png"},
+            {icon = "__base__/graphics/icons/satellite.png"}
+        },
         icon_size = 32
     }
 }
